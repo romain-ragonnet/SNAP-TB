1d59c94a5e210f6b8674cf34c3b63996d87cabc0
Romain Ragonnet <romain.ragonnet@monash.edu>
2019-08-09 09:37:41 +1000

    Update calibration_graphs.R

--- calibration_graphs.R
+++ calibration_graphs.R
@@ -38,20 +38,28 @@ plot_calibration_ranges <- function(countries){
   setwd(directory)
   tables = list()
   for (country in countries){
-    filepath = paste('lhs_calibration_',country,'/all_lhs_values.csv', sep='')
+    filepath = paste('test_LHScalibration_100s12r_Jul2019_',country,'/all_lhs_values.csv', sep='')
     tables[[country]] = read.csv(filepath,header = TRUE)
   }
-  par_names = c('TB mortality multiplier', 'g (>= 15 y.o.)', 'infectiousness switching age',
-                'g (5 to 14 y.o.)', 'g (0 to 4 y.o.)', 'transmission probability')
+  par_names = c('time from detection to treatment', 'TB mortality rate (smear-negative TB)', 'g (>= 15 y.o.)',
+                'g (0 to 4 y.o.)', 'g (5 to 14 y.o.)','transmission probability', 
+                'average number of coworkers', 'self-cure rate (smear-negative TB)', 'infectiousness switching age', 
+                'self-cure rate (smear-positive TB)', 'TB mortality rate (smear-positive TB)')
   
-  x_lims = list('1'=c(0.8, 1.2), '2'=c(0.5, 1), '3'=c(10, 20), 
-            '4'=c(0.5, 1), '5'=c(0.5, 1), '6'=c(0, 0.005))  # mortality_mult, g_adult, infectiouss age, g_teen, g_child, p_trans
+  plot_order = c(6, 9, 7, 4, 5, 3, 1, 10, 8, 11, 2)
+  
+  
+  x_lims = list('1'=c(0,14), '2'=c(0.,0.1), '3'=c(0.5,1), 
+                '4'=c(0.5, 1), '5'=c(0.5, 1), '6'=c(0, 0.005),
+                '7'=c(0,40), '8'=c(0, 0.3), '9'=c(10, 20),
+                '10' = c(0, 0.3), '11'=c(0.3,0.5))  
+  
   
   filename = 'calibration_ranges.png'
-  png(filename = filename, res=150,width = 15, height=10,units = 'in')
-  par(mfrow=c(2,3))
+  png(filename = filename, res=150,width = 15, height=20,units = 'in')
+  par(mfrow=c(4,3))
   counter = 0
-  for (j in c(6, 3, 1, 5, 4, 2)){  # for each parameter
+  for (j in plot_order){  # for each parameter
     counter = counter + 1
     plot(0,0,type='n',xlab='', ylab='',xlim=x_lims[[as.character(j)]], ylim=c(0.5, 5.5), main=par_names[j],
          cex.main=1.5, cex.axis =1.3, yaxt='n', bty='n')
@@ -61,17 +69,25 @@ plot_calibration_ranges <- function(countries){
       h = h+1
       table = tables[[country]]
       par_vals=table[,j]
-      n = table[,8]
+      index_n_samples = ncol(table)
+      n = table[,index_n_samples]
       sample_par = c()
       for (i in 1:length(n)){
         sample_par = c(sample_par, rep(par_vals[i],n[i]))
       }
       qs = quantile(sample_par, probs = c(0.025,0.5,0.975),names = FALSE)
+      
+      if (j==6){
+        print(country)
+        print(round(qs*10000,1))
+      }
+      
       segments(x0=qs[1],x1 = qs[3],y0=h,y1 = h,lwd=2)
       points(x=qs[2],y=h,cex=2,pch=16)
-      if (counter %in% c(1,4)){
+      if (counter %in% c(1,4,7,10)){
         mtext(text = country,side = 2,at = h,adj=0, las=1,line = 3, cex=1.1)
       }
+      
     }
   }
   
@@ -80,6 +96,8 @@ plot_calibration_ranges <- function(countries){
 
 
 countries = c('India', 'Indonesia', 'China', 'Philippines', 'Pakistan')
+# countries = c('India', 'Indonesia', 'Philippines')
+
 for (country in countries){
   plot_calibration_posteriors(country)
 }
